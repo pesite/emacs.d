50a7efe38162bea2bbcd9c1e8ffa7214348b1473
Patrick <PatrickTeich@pesite.de>
2015-10-11 11:34:41 +0200

    ac broken config

--- init.el
+++ init.el
@@ -15,6 +15,7 @@
 
 ;;
 (setq-default indent-tabs-mode nil)
+(add-to-list 'load-path "/home/patti/.emacs.d/lisp")
 
 ; set auto-save file handling
 ;; Save all tempfiles in $TMPDIR/emacs$UID/                                                  
@@ -58,28 +59,16 @@
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
-; auto-complete
-(add-to-list 'load-path "/home/patti/.emacs.d/lisp")
-;; (add-to-list 'load-path "/home/patti/.emacs.d/elpa/auto-complete-20150201.150")
-;; ;; (require 'auto-complete-config)
-;; (add-hook 'after-init-hook (lambda ()
-;; 			     (add-to-list 'ac-dictionary-directories "/home/patti/.emacs.d/ac-dict")
-;; 			     (ac-config-default)
-;; 			     (add-to-list 'ac-modes 'html-mode)
-;; 			     (add-to-list 'ac-modes 'nxml-mode)
-;; 			     (add-to-list 'ac-modes 'latex-mode)))
-
-;;(require 'ac-company)
-;;(ac-company-define-source ac-source-company-elisp company-elisp)
-;;(add-hook 'emacs-lisp-mode-hook
-;;     (lambda ()
-;;        (add-to-list 'ac-sources 'ac-source-company-elisp)))
-
-;; (define-key ac-completing-map "\t" 'ac-complete)
-;; (define-key ac-completing-map "\r" nil)
-
 ; tml - support ; wip
 (setq auto-mode-alist (cons '("\\.tml$" . nxml-mode) auto-mode-alist))
+
+;; auto multi-web-mode
+(setq auto-mode-alist
+      (append
+       (list (cons "\\.php$"  'multi-web-mode)
+	     ;; (cons "\\.other-extensions$"     'promela-mode)
+	     )
+       auto-mode-alist))
 
 ;; add promela mode
 (add-to-list 'load-path "/home/patti/.emacs.d/base")
@@ -201,19 +190,19 @@
 ;;      (tern-ac-setup)))
 
 ;; python IDE features
-(require 'pymacs)
-(autoload 'pymacs-apply "pymacs")
-(autoload 'pymacs-call "pymacs")
-(autoload 'pymacs-eval "pymacs" nil t)
-(autoload 'pymacs-exec "pymacs" nil t)
-(autoload 'pymacs-load "pymacs" nil t)
-(pymacs-load "ropemacs" "rope-")
-(setq ropemacs-enable-shortcuts nil) (setq ropemacs-local-prefix "C-c C-p")
+;(require 'pymacs)
+;(autoload 'pymacs-apply "pymacs")
+;(autoload 'pymacs-call "pymacs")
+;(autoload 'pymacs-eval "pymacs" nil t)
+;(autoload 'pymacs-exec "pymacs" nil t)
+;(autoload 'pymacs-load "pymacs" nil t)
+;(pymacs-load "ropemacs" "rope-")
+;(setq ropemacs-enable-shortcuts nil) (setq ropemacs-local-prefix "C-c C-p")
 
 
-(defmacro after (mode &rest body)
-  `(eval-after-load ,mode
-     '(progn ,@body)))
+;; (defmacro after (mode &rest body)
+;;   `(eval-after-load ,mode
+;;      '(progn ,@body)))
 
 ;; (after 'auto-complete
 ;;        (add-to-list 'ac-dictionary-directories "~/.emacs.d/dict")
@@ -235,8 +224,8 @@
 ;;                    (add-to-list 'ac-sources 'ac-source-ropemacs)
 ;;                    (auto-complete-mode))))
 
-(add-hook 'python-mode-hook
-          (lambda () (define-key python-mode-map (kbd "<C-tab>") 'rope-code-assist)))
+;; (add-hook 'python-mode-hook
+;;           (lambda () (define-key python-mode-map (kbd "<C-tab>") 'rope-code-assist)))
 
 ;; Configure flymake for Python
 (setq pylint "/usr/bin/epylint")
@@ -285,7 +274,7 @@
 	       nil 1 2 4))
 
 ;; yasnippet
-(add-to-list 'load-path "~/.emacs.d/elpa/yasnippet-20141223.303")
+;;(add-to-list 'load-path "~/.emacs.d/elpa/yasnippet-20141223.303")
 ;; (require 'yasnippet)
 ;; (yas-load-directory "~/.emacs.d/snippets")
 ;; (yas-global-mode 1)
@@ -328,11 +317,11 @@
 ;;; c++ ide
 ;; ggtags
 ;;(require 'ggtags)
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
-              (ggtags-mode 1)
-	      (setq speedbar-show-unknown-files t))))
+;; (add-hook 'c-mode-common-hook
+;;           (lambda ()
+;;             (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
+;;               (ggtags-mode 1)
+;; 	      (setq speedbar-show-unknown-files t))))
 
 ;;(define-key ggtags-mode-map (kbd "C-c g s") 'ggtags-find-other-symbol)
 ;;(define-key ggtags-mode-map (kbd "C-c g h") 'ggtags-view-tag-history)
@@ -348,7 +337,7 @@
 
 ;; auto-completion
 ;; (require 'company)
-(add-hook 'after-init-hook 'global-company-mode)
+;; (add-hook 'after-init-hook 'global-company-mode)
 (require 'cc-mode)
 (require 'semantic)
 
@@ -360,56 +349,65 @@
 ;;(setq company-backends 'company-c-headers)
 
 ;;;; company sample backend
-(defun create-sample-backend ()
-  (require 'company)
+;; (defun create-sample-backend ()
+;;   (require 'company)
   
-  (defconst sample-completions
-    '("java.util.jar" "sun.util" "java.util" "java.net" "sun.launcher" "sun.reflect.misc" "sun.reflect.generics.repository" "sun.net.www.protocol.jar" "sun.security.action" "java.nio.charset" "java.io" "sun.nio" "sun.net.www" "java.util.zip" "java.lang.ref" "java.lang" "sun.misc" "sun.net.www.protocol.file" "java.util.concurrent" "sun.util.locale" "java.util.concurrent.atomic" "sun.reflect.annotation" "java.nio.charset.spi" "java.lang.invoke" "java.nio" "java.util.concurrent.locks" "sun.reflect" "java.security" "sun.security.util" "sun.nio.cs" "java.lang.reflect" "java.security.cert" "sun.nio.ch" "sun.net.util"))
+;;   (defconst sample-completions
+;;     '("java.util.jar" "sun.util" "java.util" "java.net" "sun.launcher" "sun.reflect.misc" "sun.reflect.generics.repository" "sun.net.www.protocol.jar" "sun.security.action" "java.nio.charset" "java.io" "sun.nio" "sun.net.www" "java.util.zip" "java.lang.ref" "java.lang" "sun.misc" "sun.net.www.protocol.file" "java.util.concurrent" "sun.util.locale" "java.util.concurrent.atomic" "sun.reflect.annotation" "java.nio.charset.spi" "java.lang.invoke" "java.nio" "java.util.concurrent.locks" "sun.reflect" "java.security" "sun.security.util" "sun.nio.cs" "java.lang.reflect" "java.security.cert" "sun.nio.ch" "sun.net.util"))
 
-  (defun string/reverse (str)
-    "Reverse the str where str is a string"
-    (apply #'string 
-	   (reverse 
-	    (string-to-list str))))
+;;   (defun string/reverse (str)
+;;     "Reverse the str where str is a string"
+;;     (apply #'string 
+;; 	   (reverse 
+;; 	    (string-to-list str))))
   
-  (defun sample-fuzzy-match (prefix candidate)
-    (cl-subsetp (string-to-list prefix)
-		(string-to-list candidate)))
+;;   (defun sample-fuzzy-match (prefix candidate)
+;;     (cl-subsetp (string-to-list prefix)
+;; 		(string-to-list candidate)))
   
-  (defun company-sample-backend (command &optional arg &rest ignored)
-    (interactive (list 'interactive))
+;;   (defun company-sample-backend (command &optional arg &rest ignored)
+;;     (interactive (list 'interactive))
 
-    (case command
-      (interactive (company-begin-backend 'company-sample-backend))
-      (prefix (and (eq major-mode 'java-mode)
-		   (looking-back "import \.\*")
-		   (company-grab-symbol)))
-      (candidates
-       (let* ((rev-pref (string/reverse (second (split-string (thing-at-point 'line)))))
-	      (match-pos (string-match (regexp-quote ".") rev-pref)))
-	 (let ((short-pref (if match-pos (string/reverse (substring rev-pref match-pos)) "")))
-	   (mapcar (lambda (str) (substring str (length short-pref)))
-		   (remove-if-not
-		    (lambda (c) (sample-fuzzy-match (concat short-pref arg) c))
-		    sample-completions)))))
-      (no-cache 't)))
+;;     (case command
+;;       (interactive (company-begin-backend 'company-sample-backend))
+;;       (prefix (and (eq major-mode 'java-mode)
+;; 		   (looking-back "import \.\*")
+;; 		   (company-grab-symbol)))
+;;       (candidates
+;;        (let* ((rev-pref (string/reverse (second (split-string (thing-at-point 'line)))))
+;; 	      (match-pos (string-match (regexp-quote ".") rev-pref)))
+;; 	 (let ((short-pref (if match-pos (string/reverse (substring rev-pref match-pos)) "")))
+;; 	   (mapcar (lambda (str) (substring str (length short-pref)))
+;; 		   (remove-if-not
+;; 		    (lambda (c) (sample-fuzzy-match (concat short-pref arg) c))
+;; 		    sample-completions)))))
+;;       (no-cache 't)))
 
-  (add-to-list 'company-backends 'company-sample-backend))
+;;   (add-to-list 'company-backends 'company-sample-backend))
 
-(add-hook 'after-init-hook 'create-sample-backend)
+;; (add-hook 'after-init-hook 'create-sample-backend)
 
 ;; immediate completion using tab
-(global-set-key (kbd "<C-tab>") 'company-complete-common)
+;;; (global-set-key (kbd "<C-tab>") 'company-complete-common)
+(global-set-key (kbd "<C-tab>") 'auto-complete)
+;; auto-complete mode everywhere -- http://emacswiki.org/emacs/AutoComplete
+;; dirty fix for having AC everywhere
+;; (define-globalized-minor-mode real-global-auto-complete-mode
+;;   auto-complete-mode (lambda ()
+;;                        (if (not (minibufferp (current-buffer)))
+;;                          (auto-complete-mode 1))
+;;                        ))
+;; (add-hook 'after-init-hook (lambda () (real-global-auto-complete-mode t)))
+
 ;; company color
 (require 'color)
-(let ((bg (face-attribute 'default :background)))
-  (custom-set-faces
-   `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
-   `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
-   `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
-   `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
-   `(company-tooltip-common ((t (:inherit font-lock-constant-face))))))
-
+;; (let ((bg (face-attribute 'default :background)))
+;;   (custom-set-faces
+;;    `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
+;;    `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
+;;    `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
+;;    `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
+;;    `(company-tooltip-common ((t (:inherit font-lock-constant-face))))))
 
 ;;; java autocomplete
 ;;(add-to-list 'load-path "~/.emacs.d/ajc-java-complete/")
@@ -422,7 +420,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(eclim-eclipse-dirs (quote ("~/eclipse")))
+ '(eclim-executable "~/eclipse/eclim")
+ '(org-support-shift-select t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -435,13 +435,11 @@
  '(company-tooltip-selection ((t (:inherit font-lock-function-name-face)))))
 
 ;; company eclim
-(add-hook 'after-init-hook (lambda ()
-			     (require 'company-emacs-eclim)
-			     (company-emacs-eclim-setup)))
+;; (add-hook 'after-init-hook (lambda ()
+;; 			     (require 'company-emacs-eclim)
+;; 			     (company-emacs-eclim-setup)))
 
-(custom-set-variables
- '(eclim-eclipse-dirs '("~/eclipse"))
- '(eclim-executable "~/eclipse/eclim"))
+
 
 ;; config multi-web-mode
 (with-eval-after-load 'multi-web-mode
@@ -451,3 +449,103 @@
                     (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
   (setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
   (multi-web-global-mode 1))
+
+
+;; add gccsense
+(require 'gccsense)
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (local-set-key (kbd "<C-tab>") 'ac-complete-gccsense)))
+
+;; tags
+(defun create-tags (dir-name)
+  "Create tags file."
+  (interactive "DDirectory: ")
+  (eshell-command 
+   (format "find %s -type f -name \"*.[ch]\" | etags -" dir-name)))
+
+  ;;;  Jonas.Jarnestrom<at>ki.ericsson.se A smarter               
+  ;;;  find-tag that automagically reruns etags when it cant find a               
+  ;;;  requested item and then makes a new try to locate it.                      
+  ;;;  Fri Mar 15 09:52:14 2002
+(defadvice find-tag (around refresh-etags activate)
+  "Rerun etags and reload tags if tag not found and redo find-tag.              
+   If buffer is modified, ask about save before running etags."
+  (let ((extension (file-name-extension (buffer-file-name))))
+    (condition-case err
+        ad-do-it
+      (error (and (buffer-modified-p)
+                  (not (ding))
+                  (y-or-n-p "Buffer is modified, save it? ")
+                  (save-buffer))
+             (er-refresh-etags extension)
+             ad-do-it))))
+(defun er-refresh-etags (&optional extension)
+  "Run etags on all peer files in current dir and reload them silently."
+  (interactive)
+  (shell-command (format "etags *.%s" (or extension "el")))
+  (let ((tags-revert-without-query t))  ; don't query, revert silently          
+    (visit-tags-table default-directory nil)))
+
+;; load editorconfig
+(load "editorconfig")
+
+;; set fundamental mode to org mode
+(setq-default major-mode 'org-mode)
+
+
+;; auto-complete
+(add-to-list 'load-path "/home/patti/.emacs.d/elpa/auto-complete-20150618.1949")
+
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+;; (add-to-list 'load-path "/home/patti/.emacs.d/elpa/auto-complete-20150201.150")
+;; (require 'auto-complete-config)
+;; (add-hook 'after-init-hook (lambda ()
+;; 			     (add-to-list 'ac-dictionary-directories "/home/patti/.emacs.d/ac-dict")
+;; 			     (ac-config-default)
+;;                              (add-to-list 'ac-modes 'php-mode)
+;;                              (add-to-list 'ac-modes 'python-mode)
+;;                              (add-to-list 'ac-modes 'js2-mode)
+;; 			     (add-to-list 'ac-modes 'html-mode)
+;; 			     (add-to-list 'ac-modes 'nxml-mode)
+;; 			     (add-to-list 'ac-modes 'latex-mode)))
+
+;; (add-hook 'after-init-hook (lambda ()
+;;                              (require 'ac-company)
+;;                              (ac-company-define-source ac-source-company-elisp company-elisp)
+;;                              (add-hook 'emacs-lisp-mode-hook
+;;                                        (lambda ()
+;;                                          (add-to-list 'ac-sources 'ac-source-company-elisp)))))
+
+;; (define-key ac-completing-map "\t" 'ac-complete)
+;; (define-key ac-completing-map "\r" nil)
+
+
+;;; semantic auto-completion -- http://emacswiki.org/emacs/AutoCompleteSources#toc6
+(defun ac-semantic-construct-candidates (tags)
+  "Construct candidates from the list inside of tags."
+  (apply 'append
+         (mapcar (lambda (tag)
+                   (if (listp tag)
+                       (let ((type (semantic-tag-type tag))
+                             (class (semantic-tag-class tag))
+                             (name (semantic-tag-name tag)))
+                         (if (or (and (stringp type)
+                                      (string= type "class"))
+                                 (eq class 'function)
+                                 (eq class 'variable))
+                             (list (list name type class))))))
+                 tags)))
+
+
+(defvar ac-source-semantic-analysis nil)
+(setq ac-source-semantic
+      `((sigil . "b")
+        (init . (lambda () (setq ac-source-semantic-analysis
+                                 (condition-case nil
+                                     (ac-semantic-construct-candidates (semantic-fetch-tags))))))
+        (candidates . (lambda ()
+                        (if ac-source-semantic-analysis
+                            (all-completions ac-target (mapcar 'car ac-source-semantic-analysis)))))))
