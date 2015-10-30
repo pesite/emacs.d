fbd2d2bf694c2919cedb60754ff44a7deab37bd3
Patrick Teich <PatrickTeich@pesite.de>
2015-10-30 11:41:27 +0100

    Set js2-mode offset to 2

--- lisp/init-js.el
+++ lisp/init-js.el
@@ -1,5 +1,6 @@
 ;; replace js-mode by js2-mode for proper indentation
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(setq-default js2-basic-offset 2)
 
 (provide 'init-js)
