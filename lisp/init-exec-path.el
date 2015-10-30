ee9950c13546cb9e44d5a0984c41e519b585a3f2
Patrick Teich <PatrickTeich@pesite.de>
2015-10-30 11:41:01 +0100

    Add GOROOT and GOPATH environment

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -1,7 +1,7 @@
 (require-and-load-package 'exec-path-from-shell)
 
 (eval-after-load 'exec-path-from-shell
-  (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"))
+  (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE" "GOPATH" "GOROOT"))
     (add-to-list 'exec-path-from-shell-variables var)))
 
 (when (memq window-system '(mac ns x))
