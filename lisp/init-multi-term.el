ffd8342d7cef8f86bd8cfc94addaa3a52b8ca4bd
Patrick Teich <PatrickTeich@pesite.de>
2016-01-06 22:01:57 +0100

    Customize multi-term bindings

--- lisp/init-multi-term.el
+++ lisp/init-multi-term.el
@@ -1,7 +1,10 @@
 (require-package 'multi-term)
 
 ;; Disable yasnippet, so we can tab-complete
+;; Enable term-line-mode binding
 (add-hook 'term-mode-hook (lambda()
-                            (yas-minor-mode -1)))
+                            (yas-minor-mode -1)
+                            (add-to-list 'term-bind-key-alist '("C-c C-j" . term-line-mode))
+                            (cua-mode 1)))
 
 (provide 'init-multi-term)
