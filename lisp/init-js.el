a35dfb4a1932da71584fee2804eba85c8e6f38a5
Patrick Teich <PatrickTeich@pesite.de>
2016-03-23 20:25:13 +0100

    Add alias for js2-basic-offset, so it's the same on every javascript mode

--- lisp/init-js.el
+++ lisp/init-js.el
@@ -1,6 +1,7 @@
 ;; replace js-mode by js2-mode for proper indentation
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(setq-default js2-basic-offset 2)
+;;(setq-default js2-basic-offset 2)
+(defvaralias 'js2-basic-offset 'js-indent-level)
 
 (provide 'init-js)
