3ca883552b26b78d447b0e3a3a53d60355952d33
Patrick Teich <PatrickTeich@pesite.de>
2018-02-01 21:03:36 +0100

    Update to use js2 mode again

--- init.el
+++ init.el
@@ -3,6 +3,13 @@
 
 ;;; Code:
 ;; add .emacs.d/lisp to load-path
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "base" user-emacs-directory))
 
--- lisp/init-js.el
+++ lisp/init-js.el
@@ -1,10 +1,10 @@
-;;; package -- provide js3 mode for javascript.
+;;; package -- provide js2 mode for javascript.
 ;;; Commentary:
 
 ;;; Code:
-(require-and-load-package 'js3-mode)
+(require-and-load-package 'js2-mode)
 
-(add-to-list 'auto-mode-alist '("\\.js$" . js3-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 (provide 'init-js)
 ;;; init-js.el ends here
